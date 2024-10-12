6b19b43812e14e987b8af6d74fb1a8296648f403
PaoloFabris98 <f98paolo@gmail.com>
2024-10-12 12:29:02 +0200

    feat: idk

--- js/main.js
+++ js/main.js
@@ -15,26 +15,26 @@ for(let i = start; i <= interaction; i++){
     if((i%fizz === 0) && (i%Buzz === 0)){
         //se multipli di 3 e di 5
         console.log(i + ". " + fizzWord + buzzWord);
-        row.innerHTML += `<div class="col-3">
+        row.innerHTML += `<div class="col-2 bg-success p-2 text-white bg-opacity-75 rounded-pill border ">
                             ${i} ${fizzWord}${buzzWord}
                         </div>`
     } else if(i%Buzz === 0){
         //se multipli di 5
         console.log(i + ". " + buzzWord);
-        row.innerHTML += `<div class="col-3">
+        row.innerHTML += `<div class="col-2 col-2 bg-success p-2 text-white bg-opacity-75 rounded-pill border ">
                             ${i} ${buzzWord}
                         </div>`
     } else if (i%fizz === 0){
         // se multipli di 3
         
         console.log(i + ". " + fizzWord);
-        row.innerHTML += `<div class="col-3">
+        row.innerHTML += `<div class="col-2 col-2 bg-success p-2 text-white bg-opacity-75 rounded-pill border ">
                             ${i} ${fizzWord}
                         </div>`
     } else {
         //tutti gli altri
         console.log(i + ".");
-        row.innerHTML += `<div class="col-3">
+        row.innerHTML += `<div class="col-2 col-2 bg-success p-2 text-white bg-opacity-75 rounded-pill border ">
                             ${i}
                         </div>`
     };
