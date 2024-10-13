f0fa0891ee9ed6e640cb674dab41c9d2891992f7
PaoloFabris98 <f98paolo@gmail.com>
2024-10-13 12:44:23 +0200

    feat:betters

--- js/main.js
+++ js/main.js
@@ -2,15 +2,15 @@
 // varriabili
 const buttonSubmit = document.getElementById("submit");
 const boxs = document.getElementById("main-row");
-let interaction = document.getElementById("itterations") ;
-const start = 0;
+const allert = document.getElementById("Alert");
 let fizz = document.getElementById("divisore1");
-const fizzWord = "Fizz";
 let Buzz = document.getElementById("divisore2");
+
+const start = 0;
+
+let interaction = document.getElementById("itterations") ;
+const fizzWord = "Fizz";
 const buzzWord = "Buzz";
-
-
-const row = document.getElementById("main-row");
 
 buttonSubmit.addEventListener("click", function (event) {
     event.preventDefault();
@@ -19,26 +19,36 @@ buttonSubmit.addEventListener("click", function (event) {
     let num1 = Number(fizz.value);
     let num2 = Number(Buzz.value);
 
-    for(let i = start; i <= it; i++){
+    if (isNaN(it) || isNaN(num1) || isNaN(num2)) {
+        boxs.classList.add("d-none");
+        allert.classList.remove("d-none");
+        allert.innerHTML = `
+        In tutte e 3 le caselle devono esserci solo numeri.
+        `;
+    } else {
+        allert.classList.add("d-none");
         boxs.classList.remove("d-none");
-        row.innerHTML += `<div class="col-2" id="inserted-case-${i}"><div class="bg-success p-3 text-white text-center rounded-pill mt-2 border" id="inserted-${i}"></div></div>`;
-        let inserted = document.getElementById(`inserted-${i}`);
-        if((i%num1 === 0) && (i%num2 === 0)){
-            //se multipli di 3 e di 5
-            console.log(i + ". " + fizzWord + buzzWord);
-            inserted.innerHTML += `${i} ${fizzWord}${buzzWord}`
-        } else if(i%num2 === 0){
-            //se multipli di 5
-            console.log(i + ". " + buzzWord);
-            inserted.innerHTML += `${i} ${buzzWord}`;
-        } else if (i%num1 === 0){
-            // se multipli di 3
-            console.log(i + ". " + fizzWord);
-            inserted.innerHTML += `${i} ${fizzWord}`;
-        } else {
-            //tutti gli altri
-            console.log(i + ".");
-            inserted.innerHTML += `${i}`;
+        boxs.innerHTML = ''; // Reset contenuto per evitare duplicati
+        for (let i = start; i <= it; i++) {
+            boxs.innerHTML += `<div class="col-2" id="inserted-case-${i}"><div class="bg-success p-3 text-white text-center rounded-pill mt-2 border" id="inserted-${i}"></div></div>`;
+            let inserted = document.getElementById(`inserted-${i}`);
+            if ((i % num1 === 0) && (i % num2 === 0)) {
+                // se multipli di 3 e di 5
+                console.log(i + ". " + fizzWord + buzzWord);
+                inserted.innerHTML += `${i} ${fizzWord}${buzzWord}`;
+            } else if (i % num2 === 0) {
+                // se multipli di 5
+                console.log(i + ". " + buzzWord);
+                inserted.innerHTML += `${i} ${buzzWord}`;
+            } else if (i % num1 === 0) {
+                // se multipli di 3
+                console.log(i + ". " + fizzWord);
+                inserted.innerHTML += `${i} ${fizzWord}`;
+            } else {
+                // tutti gli altri
+                console.log(i + ".");
+                inserted.innerHTML += `${i}`;
+            };
         };
     };
 });
